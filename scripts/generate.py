b872d5f12c8d9d86bba286c50372f24cc12fea7d
Aang23 <qwerty15@gmx.fr>
2019-07-07 20:50:11 +0200

    Generate sand loot

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,3 +1,4 @@
 import loot_tables
 
 loot_tables.generate_gravel_loot()
+loot_tables.generate_sand_loot()
--- scripts/loot_tables.py
+++ scripts/loot_tables.py
@@ -20,3 +20,22 @@ def generate_gravel_loot():
             text_file = open(folder + block_name + ".json", "w")
             text_file.write(final_file)
             text_file.close()
+
+def generate_sand_loot():
+    folder = "build/resources/main/data/undergroundbiomes/loot_tables/blocks/"
+
+    data = 0
+    with open('resources_templates/loot_tables/basic.json', 'r') as file:
+        data = file.read()
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    for stype in ub_enums.ubc_types_and_variants:
+        for variant in ub_enums.ubc_types_and_variants[stype]:
+            block_name = stype + "_sand_" + variant
+            final_file = data.replace("blocktodrop", "undergroundbiomes:" + block_name)
+            print(block_name)
+            text_file = open(folder + block_name + ".json", "w")
+            text_file.write(final_file)
+            text_file.close()
