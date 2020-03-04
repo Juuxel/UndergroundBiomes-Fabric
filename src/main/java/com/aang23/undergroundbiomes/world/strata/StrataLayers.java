ebdf7943e6a6ec8ae6989efee91ed1b4a030df58
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-03-04 17:44:14 +0200

    Clean StrataLayers
    Still needs a *lot* of cleaning, but this is a start.

--- src/main/java/com/aang23/undergroundbiomes/world/strata/StrataLayers.java
+++ src/main/java/com/aang23/undergroundbiomes/world/strata/StrataLayers.java
@@ -1,6 +1,7 @@
 package com.aang23.undergroundbiomes.world.strata;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.aang23.undergroundbiomes.UBBlocks;
 import com.aang23.undergroundbiomes.api.enums.IgneousVariant;
@@ -21,50 +22,52 @@ final class StrataLayers {
 
   public StrataLayers(WorldConfig config) {
     this.config = config;
-    layers = new StrataLayer[30][];
-    if (config.harmoniousStrata()) 
-      createHarmoniousLayers();
-    else
-    createLayers();
-    cleanup();
+    List<StrataLayer[]> layerBuilder = config.harmoniousStrata() ? createHarmoniousLayers() : createLayers();
+    layers = cleanup(layerBuilder);
   }
 
-  private void createLayers() {
-    layers[0] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 30, 32), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHALK), 38, 41), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.GREYWACKE), 65, 70)};
-    layers[1] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SHALE), 33, 36), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 38, 41), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.DOLOMITE), 60, 62), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHERT), 75, 80)};
-    layers[2] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 30, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 26, 29), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 70, 74)};
-    layers[3] = new StrataLayer[]{new StrataLayer(Blocks.SANDSTONE, 40, 43), new StrataLayer(Blocks.SAND, 44, 47), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 55, 57)};
-    layers[4] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHERT), 29, 33), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.DOLOMITE), 42, 44), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 90, 105)};
-    layers[5] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SHALE), 33, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 38, 41), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.GREYWACKE), 72, 79)};
-    layers[6] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 30, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 39, 44), new StrataLayer(Blocks.SANDSTONE, 52, 54), new StrataLayer(Blocks.SAND, 55, 60)};
-    layers[7] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 33, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 45, 49), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.GREYWACKE), 80, 85)};
-    layers[8] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 30, 32), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 70, 74), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 75, 79)};
-    layers[9] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SHALE), 31, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.DOLOMITE), 39, 42), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHERT), 62, 65)};
-    layers[10] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, IgneousVariant.RHYOLITE), 12, 18), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, IgneousVariant.KOMATIITE), 24, 29), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 80, 85)};
-    layers[11] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, IgneousVariant.BASALT), 13, 22), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.SOAPSTONE), 29, 36), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SILTSTONE), 80, 128)};
-
+  private List<StrataLayer[]> createLayers() {
+    List<StrataLayer[]> builder = new ArrayList<>();
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 30, 32), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHALK), 38, 41), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.GREYWACKE), 65, 70)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SHALE), 33, 36), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 38, 41), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.DOLOMITE), 60, 62), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHERT), 75, 80)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 30, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 26, 29), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 70, 74)});
+    builder.add(new StrataLayer[]{new StrataLayer(Blocks.SANDSTONE, 40, 43), new StrataLayer(Blocks.SAND, 44, 47), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 55, 57)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHERT), 29, 33), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.DOLOMITE), 42, 44), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 90, 105)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SHALE), 33, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 38, 41), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.GREYWACKE), 72, 79)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 30, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 39, 44), new StrataLayer(Blocks.SANDSTONE, 52, 54), new StrataLayer(Blocks.SAND, 55, 60)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 33, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 45, 49), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.GREYWACKE), 80, 85)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 30, 32), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 70, 74), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 75, 79)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SHALE), 31, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.DOLOMITE), 39, 42), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHERT), 62, 65)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, IgneousVariant.RHYOLITE), 12, 18), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, IgneousVariant.KOMATIITE), 24, 29), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 80, 85)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, IgneousVariant.BASALT), 13, 22), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.SOAPSTONE), 29, 36), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SILTSTONE), 80, 128)});
+    return builder;
   }
 
-  private void createHarmoniousLayers() {
-    layers[0] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 30, 32), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 38, 41), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.GREYWACKE), 65, 70)};
-    layers[5] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SHALE), 33, 36), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 38, 41), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.DOLOMITE), 60, 62), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHERT), 75, 80)};
-    layers[2] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 30, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 26, 29), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 70, 74)};
-    layers[3] = new StrataLayer[]{new StrataLayer(Blocks.SANDSTONE, 40, 43), new StrataLayer(Blocks.SAND, 44, 47), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 55, 57)};
-    layers[4] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHERT), 29, 33), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.DOLOMITE), 42, 44), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 90, 105)};
-    layers[1] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SHALE), 33, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 38, 41), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.GREYWACKE), 72, 79)};
-    layers[6] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 30, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 39, 44), new StrataLayer(Blocks.SANDSTONE, 52, 54), new StrataLayer(Blocks.SAND, 55, 60)};
-    layers[7] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 33, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 45, 49), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 80, 85)};
-    layers[8] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 30, 32), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 70, 74), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.GREYWACKE), 75, 79)};
-    layers[9] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SHALE), 31, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.DOLOMITE), 39, 42), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHERT), 62, 65)};
-    layers[10] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, IgneousVariant.RHYOLITE), 12, 18), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, IgneousVariant.KOMATIITE), 24, 29), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 80, 85)};
-    layers[11] = new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, IgneousVariant.BASALT), 13, 22), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.SOAPSTONE), 29, 36), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SILTSTONE), 80, 128)};
-
+  private List<StrataLayer[]> createHarmoniousLayers() {
+    List<StrataLayer[]> builder = new ArrayList<>();
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 30, 32), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 38, 41), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.GREYWACKE), 65, 70)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SHALE), 33, 36), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 38, 41), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.DOLOMITE), 60, 62), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHERT), 75, 80)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 30, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 26, 29), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 70, 74)});
+    builder.add(new StrataLayer[]{new StrataLayer(Blocks.SANDSTONE, 40, 43), new StrataLayer(Blocks.SAND, 44, 47), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 55, 57)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHERT), 29, 33), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.DOLOMITE), 42, 44), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 90, 105)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SHALE), 33, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 38, 41), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.GREYWACKE), 72, 79)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 30, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 39, 44), new StrataLayer(Blocks.SANDSTONE, 52, 54), new StrataLayer(Blocks.SAND, 55, 60)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 33, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 45, 49), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIGNITE), 80, 85)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.LIMESTONE), 30, 32), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 70, 74), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.GREYWACKE), 75, 79)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SHALE), 31, 35), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.DOLOMITE), 39, 42), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.CHERT), 62, 65)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, IgneousVariant.RHYOLITE), 12, 18), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, IgneousVariant.KOMATIITE), 24, 29), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.MARBLE), 80, 85)});
+    builder.add(new StrataLayer[]{new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, IgneousVariant.BASALT), 13, 22), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, MetamorphicVariant.SOAPSTONE), 29, 36), new StrataLayer(UBBlocks.getBlock(UBStoneStyle.STONE, SedimentaryVariant.SILTSTONE), 80, 128)});
+    return builder;
   }
 
-  private void cleanup() {
-    for (int i = 0; i < 12; i++) {
-      layers[i] = cleanedLayers(layers[i]);
+  private StrataLayer[][] cleanup(List<StrataLayer[]> layerBuilder) {
+    StrataLayer[][] result = new StrataLayer[layerBuilder.size()][];
+
+    for (int i = 0; i < layerBuilder.size(); i++) {
+      layers[i] = cleanedLayers(layerBuilder.get(i));
     }
+
+    return result;
   }
 
   private StrataLayer[] cleanedLayers(StrataLayer[] toClean) {
@@ -75,12 +78,7 @@ final class StrataLayers {
         kept.add(toClean[i]);
       }
     }
-    StrataLayer[] result = new StrataLayer[kept.size()];
-    int i = 0;
-    for (StrataLayer layer : kept) {
-      result[i++] = layer;
-    }
-    return result;
+    return kept.toArray(new StrataLayer[0]);
   }
 
 }
